0434b7691f61a41653596857fef93f180d6bb459
Salitha Chathuranga <salithachathuranga94@gmail.com>
2019-07-15 21:02:23 +0530

    add props to functional component

--- src/App.js
+++ src/App.js
@@ -13,17 +13,18 @@ function updateAnimal() {
   store.dispatch(update_animal);
 }
 
-function App() {
+function App(props) {
+  console.log(props)
   return (
     <div className="App">
       <div className="container">
         <h2 className="textCenter">Redux App</h2>
         <hr/>
-        Person: {store.getState().person.name}
+        Person: {props.person.name}
         <br/>
         <button className="btn btn-primary btn-sm" onClick={updatePerson}>update person</button>
         <br/><br/>
-        Animal: {store.getState().animal.name}
+        Animal: {props.animal.name}
         <br/>
         <button className="btn btn-primary btn-sm" onClick={updateAnimal}>update animal</button>
       </div>
